15bc115e6fb0fcbe3a981c3c14c7da20f79814c6
kieutranthithuy <kieu.tranthithuy@vti.com.vn>
2019-03-20 17:29:45 +0700

    Addtional 20/03/2019

---
+++ src/org/webdriver/test/login/PageObject.java
@@ -0,0 +1,6 @@
+package org.webdriver.test.login;
+
+public class PageObject {
+	public String Url = "http://mobilestore01.herokuapp.com/login";
+
+}
---
+++ src/org/webdriver/test/register/PageObject.java
@@ -0,0 +1,6 @@
+package org.webdriver.test.register;
+
+public class PageObject {
+	public String Url = "http://mobilestore01.herokuapp.com/register";
+
+}
